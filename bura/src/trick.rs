858a6d549431ff8a4322eec03e0f8c98bb3f896b
Dominik <dominik@lameter.com>
2022-05-31 14:20:31 -0500

    feat(trick): clean up option use

--- bura/src/trick.rs
+++ bura/src/trick.rs
@@ -3,22 +3,22 @@ use crate::Card;
 #[derive(Debug)]
 pub struct Trick {
     pub lead: Option<Vec<Card>>,
-    pub follow: Option<Vec<Card>>
+    pub follow: Option<Vec<Card>>,
 }
 
 impl Trick {
     pub fn new() -> Trick {
         Trick {
             lead: None,
-            follow: None
+            follow: None,
         }
     }
 
     pub fn leading_suit(&self) -> Option<String> {
-        return match self.lead.and_then(|lead| lead.first()) {
+        return match self.lead.as_ref().and_then(|lead| lead.first()) {
             Some(card) => Some(card.suit.clone()),
-            None => None
-        }
+            None => None,
+        };
     }
 
     pub fn play(&mut self, cards: Vec<Card>) {
@@ -32,14 +32,14 @@ impl Trick {
     }
 
     pub fn take_cards(&mut self) -> Vec<Card> {
-        let cards = Vec::new();
+        let mut cards = Vec::new();
 
-        if let Some(lead_cards) = self.lead {
+        if let Some(mut lead_cards) = self.lead.take() {
             cards.append(&mut lead_cards);
             self.lead = None;
         }
 
-        if let Some(follow_cards) = self.follow {
+        if let Some(mut follow_cards) = self.follow.take() {
             cards.append(&mut follow_cards);
             self.follow = None;
         }
