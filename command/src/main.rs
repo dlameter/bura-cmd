2b3ba4874a7a04f294887ae0e71adc81fe2537a9
Dominik <dominik@lameter.com>
2022-05-31 11:26:15 -0500

    feat(command): build example game with basic steps

--- command/src/main.rs
+++ command/src/main.rs
@@ -1,10 +1,15 @@
 use bura::game::{ GameManager, GameState };
 use bura::deck::shuffler;
 use bura::deck::builder;
+use bura::game::step;
 
 fn main() {
     let state = GameState::new(Vec::new(), builder::bura_deck());
     let mut manager = GameManager::new(state, shuffler::shuffle);
+    manager.add_step(step::draw);
+    manager.add_step(step::change_player);
+    manager.add_step(step::draw);
+    manager.add_step(step::change_player);
     
     manager.start();
 }
