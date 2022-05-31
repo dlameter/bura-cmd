e3fd941384f3f25cb6446398fec8918bab56b74b
Dominik <dominik@lameter.com>
2022-05-31 13:06:51 -0500

    feat(card): implement Display

--- bura/src/card.rs
+++ bura/src/card.rs
@@ -1,5 +1,13 @@
+use std::fmt::Display;
+
 #[derive(Debug)]
 pub struct Card {
     pub suit: String,
     pub value: String
+}
+
+impl Display for Card {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        formatter.write_fmt(format_args!("[{}|{}]", self.suit, self.value))
+    }
 }
